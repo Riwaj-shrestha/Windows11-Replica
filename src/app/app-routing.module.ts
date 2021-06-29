66113c4e9b7b604353725c6530ab0ee4856741b8
Riwaj Shrestha <rshrestha@outcodesoftware.com>
2021-06-29 21:43:33 +0545

    Add routing for main-desktop, Remove preinstalled layout and styles

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainDesktopComponent } from './windows11/main-desktop/main-desktop.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: MainDesktopComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainDesktopComponent } from './windows11/main-desktop/main-desktop.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainDesktopComponent
   ],
   imports: [
     BrowserModule,
